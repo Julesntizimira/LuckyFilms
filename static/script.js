297f6bde2189e213899461a35356c79d0a795144
Julesntizimira <ntizimijules5@gmail.com>
2024-05-18 12:50:28 +0200

    lucky correction

--- static/script.js
+++ static/script.js
@@ -1,4 +1,14 @@
 const serviceLeft = document.querySelector(".left");
+const companyName =  document.querySelector(".home-span-one");
+const welcomeMsg =  document.querySelector(".home-span-two");
+const socialMedias = document.querySelector(".social-medias")
+const homeBtn = document.querySelector(".home-btn")
+const aboutContainer = document.querySelector('.about-container');
+const workTopContainer  = document.querySelector('.work-top-container');
+const pilotContent = document.querySelector('.pilot-content');
+const teamContent = document.querySelector('.team-content');
+
+
 const options = {
     root: null,
     threshold: 1,
@@ -14,7 +24,17 @@ const observer = new IntersectionObserver(function(entries, observer) {
     })
 }, options)
 
-observer.observe(serviceLeft);
+// observer.observe(serviceLeft);
+// observer.observe(companyName);
+// observer.observe(welcomeMsg);
+// observer.observe(socialMedias);
+// observer.observe(homeBtn);
+observer.observe(aboutContainer);
+observer.observe(workTopContainer);
+observer.observe(pilotContent);
+observer.observe(teamContent);
+
+
 
 
 // const pathTwoCine = document.querySelector('.path-two-cine');
@@ -35,3 +55,47 @@ observer.observe(serviceLeft);
 //         HanleCineOpacity(path)
 //     })
 // }, 1000);
+
+
+// const observer2 = new IntersectionObserver(function(entries, observer2) {
+//     entries.forEach(entry => {
+//         if (!entry.isIntersecting) {
+//             return;
+//         }
+//         document.querySelector('.sunset').classList.toggle('hide');
+//     })
+// }, options)
+
+// observer2.observe(document.querySelector('footer'))
+
+document.addEventListener('DOMContentLoaded', function() {
+    const home = document.querySelector('.home');
+    const sunset = document.querySelector('.sunset');
+    
+
+    window.addEventListener('scroll', function() {
+        if (window.scrollY > 1000) { 
+            home.classList.add('hide');
+            sunset.classList.remove('hide');
+        } else {
+            home.classList.remove('hide');
+            sunset.classList.add('hide');
+        }
+    });
+});
+
+const menu = document.querySelector('.menu');
+const navHidden = document.querySelector('.nav-ul-hidden');
+const navItems = document.querySelectorAll('.nav-ul-hidden a');
+
+
+
+menu.addEventListener('click', () => {
+    navHidden.classList.toggle('visible');
+});
+
+navItems.forEach(item => {
+    item.addEventListener('click', () => {
+        navHidden.classList.toggle('visible');
+    })
+})
